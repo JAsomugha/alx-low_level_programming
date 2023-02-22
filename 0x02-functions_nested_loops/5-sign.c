bb38ffd8a785bea4a652d188d22be70770469d28
JAsomugha <jasomugha68@hotmail.com>
2023-02-21 17:49:32 -0800

    Checking for sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_sign - prints the sign of a number
+ *@c: the number of the sign to be checked
+ *
+ *Return: 1 if number is greater than zero, 0 if number is zero, -1 if number is less than zero.
+ */
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		_putchar('+');
+		return ('1');
+	}
+	else if (c == 0)
+	{
+		_putchar('0');
+		return ('0');
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
