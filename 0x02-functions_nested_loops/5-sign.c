d8c519941bc2219b6b718b6ccb44a79f988db5fa
JAsomugha <jasomugha68@hotmail.com>
2023-02-21 18:07:33 -0800

    Check for signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,19 +3,21 @@
  *print_sign - prints the sign of a number
  *@c: the number of the sign to be checked
  *
- *Return: 1 if number is greater than zero, 0 if number is zero, -1 if number is less than zero.
+ *Return: 1 if number is greater than zero
+ *0 if number is zero
+ *-1 if number is less than zero.
  */
 int print_sign(int c)
 {
 	if (c > 0)
 	{
 		_putchar('+');
-		return ('1');
+		return (1);
 	}
 	else if (c == 0)
 	{
 		_putchar('0');
-		return ('0');
+		return (0);
 	}
 	else
 	{
