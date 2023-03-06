cf201d76753e244fa3e810cd6708410dcf465d20
JAsomugha <jasomugha68@hotmail.com>
2023-03-06 04:05:53 -0800

    function that locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - function that fills memory with a constant byte.
+ * @s: string destination
+ * @c: character to replace
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int j = 0;
+
+	for (; s[j] != '\0' && s[j] != c; j++)
+	{
+		continue;
+	}
+	if (s[j] == c)
+		return (s + j);
+	else
+		return (NULL);
+}
