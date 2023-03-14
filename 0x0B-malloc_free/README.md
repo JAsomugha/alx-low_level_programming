961cb84af2193cd7ace1f9568db5c8ae694ff1fa
JAsomugha <jasomugha68@hotmail.com>
2023-03-14 16:29:08 -0700

    Function that splits a string into words

---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+0x0B-malloc-free
+Memory Allocation
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdlib.h>
+
+char **strtow(char *str);
+int split(char *str);
+int _strlen(char *str);
+
+#endif
